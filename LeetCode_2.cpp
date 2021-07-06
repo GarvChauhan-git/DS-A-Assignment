4b1554388aac615958f6ad5b3ac11fb4aeaa9d0d
Garv Chauhan <71927022+GarvChauhan-git@users.noreply.github.com>
2021-07-06 20:24:42 +0530

    Add files via upload

---
+++ LeetCode_2.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std ;
+
+int removeElement( vector<int>&arr, int val )
+{
+    int k=0,i=0;
+    int n = arr.size();
+    for(i=0; i<n ; i++)
+    {
+        if(arr[i]!= val)
+        {
+            arr[k] = arr[i];
+            k++;
+        }
+    }
+    return k ;
+}
+
+int main()
+{
+    int v;
+    cout<<"Enter the value to remove";
+    cin>>v;
+    vector<int>arr;
+    arr.push_back(3);
+    arr.push_back(2);
+    arr.push_back(2);
+    arr.push_back(3);
+    int N = removeElement( arr, v);
+    cout<<N;
+}
