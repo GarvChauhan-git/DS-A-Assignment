0904ae54a21439e60a811b543e47ca13d6f13978
Garv Chauhan <71927022+GarvChauhan-git@users.noreply.github.com>
2021-07-08 01:25:00 +0530

    Q10.  Decompress Run-Length Encoded List

---
+++ LeetCode_10.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> decompressRLElist(vector<int>& nums) {
+        vector<int>res;
+        for(int i=0 ;i<nums.size()/2 ; i++)
+        {
+            int n = nums[2*i];
+            for(int k=0 ;k<n ;k++)
+            {
+                res.push_back(nums[2*i+1]);
+            }
+        }
+        return res ;
+    }
+};
